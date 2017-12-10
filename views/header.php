28c61c8a7bafa7ac7e730dc097a985f92d755c73
eva <evapamfil1@gmail.com>
2017-12-10 22:40:35 +0100

    add header

---
+++ views/header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--TYPOGRAPHIES-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+          rel="stylesheet">
+
+    <!--FONT AWESOME-->
+    <script src="https://use.fontawesome.com/75a87d192f.js"></script>
+
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+
+    <title>header</title>
+
+    <style>
+        .logo{
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <nav class="navbar  navbar-light bg-light justify-content-between">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <img class="logo" src="../assets/pictures/amazon-dark.svg">
+        <div class="d-flex">
+            <a class="navbar-brand d-flex flex-column align-items-center" href="#"><i class="fa fa-user" aria-hidden="true"></i>Profil</a>
+            <a class="navbar-brand d-flex flex-column align-items-center" href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Panier</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Boutique de noël</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Amazon prime video</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Amazon music</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Prime photo et drive</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Liseuses Kindle & ebooks</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Appstore pour Android</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Livre & Audible</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Musique, DVD et Blu-ray</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Jeux vidéo et Consoles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">High-Tech et Informatique</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Jouets, Enfants et Bébés</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Maison, Bricolage, Animalerie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Beauté, Santé, Epicerie</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Vetements, Chaussures, Bijoux</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sports et Loisirs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Auto et moto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Commerce, Industrie et Sciences</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Handmade</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Amazon Launchpad</a>
+                </li>
+
+
+            </ul>
+        </div>
+    </nav>
+</header>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
+        integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
+        integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+        crossorigin="anonymous"></script>
+</body>
+</html>
