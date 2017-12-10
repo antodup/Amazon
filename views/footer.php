b59cb3b70d5be53078523cc30a0e88f4680cdacd
antodup <anthony_dupre@live.fr>
2017-12-10 15:40:17 +0100

    footer

--- views/footer.php
+++ views/footer.php
@@ -11,23 +11,77 @@
           rel="stylesheet">
 
     <!--BOOTSTRAP-->
-    <link rel="stylesheet" href="../assets/css/main.css">
-    <link rel="stylesheet" href="../assets/css/bootstrap-reboot.css">
     <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
 
     <title>footer</title>
 </head>
+
 <body>
+<footer class="footer-page p-4">
+    <a href="#"><img class="logo_footer rounded mx-auto d-block mb-5" src="../assets/pictures/amazon-dark.svg"
+                     alt="logo"></a>
+    <section class="container">
+        <article class="row">
+            <div class="list-group col-md-3">
+                <span class="title-list-footer list-group-item ">Pour mieux nous connaître</span>
 
+                <a href="#" class="list-group-item list-group-item-action">À propos d'Amazon</a>
+                <a href="#" class="list-group-item list-group-item-action">Carrières</a>
+                <a href="#" class="list-group-item list-group-item-action">Amazon et notre planète</a>
+                <a href="#" class="list-group-item list-group-item-action">Amazon Ensemble</a>
+            </div>
+            <div class="list-group col-md-3">
+                <span class="list-group-item title-list-footer">Gagnez de l'argent</span>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
-            integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
-            crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
-            integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
-            crossorigin="anonymous"></script>
+                <a href="#" class="list-group-item list-group-item-action">Vendez sur Amazon</a>
+                <a href="#" class="list-group-item list-group-item-action">Devenez Partenaire</a>
+                <a href="#" class="list-group-item list-group-item-action">Expédié par Amazon</a>
+                <a href="#" class="list-group-item list-group-item-action">Faites la promotion de vos produits</a>
+                <a href="#" class="list-group-item list-group-item-action">Auto-publiez votre livre</a>
+                <a href="#" class="list-group-item list-group-item-action">Amazon Pay</a>
+                <a href="#" class="list-group-item list-group-item-action">Devenez fournisseur</a>
+                <a href="#" class="list-group-item list-group-item-action">Tous nos programmes</a>
+            </div>
+            <div class="list-group col-md-3">
+                <span class="list-group-item title-list-footer">Moyens de paiement Amazon</span>
+
+                <a href="#" class="list-group-item list-group-item-action">Cartes de paiement</a>
+                <a href="#" class="list-group-item list-group-item-action">Amazon Currency Converter</a>
+                <a href="#" class="list-group-item list-group-item-action">Chèques-cadeaux</a>
+                <a href="#" class="list-group-item list-group-item-action">Recharge en ligne</a>
+                <a href="#" class="list-group-item list-group-item-action">Recharge en magasin</a>
+            </div>
+            <div class="list-group col-md-3">
+                <span class="list-group-item  title-list-footer">Besoin d'aide ?</span>
+
+                <a href="#" class="list-group-item list-group-item-action">Visualiser ou suivre vos commandes</a>
+                <a href="#" class="list-group-item list-group-item-action">Tarifs et options de livraison</a>
+                <a href="#" class="list-group-item list-group-item-action">Amazon Prime</a>
+                <a href="#" class="list-group-item list-group-item-action">Retours et remplacements</a>
+                <a href="#" class="list-group-item list-group-item-action">Application Amazon Mobile</a>
+                <a href="#" class="list-group-item list-group-item-action">Amazon Assistant</a>
+                <a href="#" class="list-group-item list-group-item-action">Aide</a>
+            </div>
+        </article>
+    </section>
+</footer>
+<section class="legal p-4 d-flex justify-content-center">
+    <a class="m-2" href="#">Conditions générales de vente</a>
+    <a class="m-2" href="#">Vos informations personnelles</a>
+    <a class="m-2" href="#">Cookies et Publicité sur Internet</a>
+    <span class="m-2">© 1996-2017, Amazon.com, Inc. ou ses filiales.</span>
+</section>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
+        integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
+        integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+        crossorigin="anonymous"></script>
 </body>
 </html>
