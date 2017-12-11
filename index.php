9f2010722e81705ae9375aa7b63976258e980185
MatthieuLemaire <lemairematthieudecergywanadoo.fr>
2017-12-11 09:59:52 +0100

    MAJ index.php

---
+++ index.php
@@ -0,0 +1,200 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--TYPOGRAPHIES-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+          rel="stylesheet">
+
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <title>Amazon</title>
+</head>
+<body>
+    <?php
+        include ("views/header.php")
+    ?>
+    <section class="d-flex justify-content-center align-items-center vh100">
+        <div class="col-8">
+            <nav class="navbar navbar-light bg-light d-flex flex-nowrap">
+                <form class="form-inline w-100 d-flex flex-nowrap col-12">
+                    <input class="form-control mr-sm-2 col-10" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0 col-2" type="submit">Search</button>
+                </form>
+            </nav>
+        </div>
+        <div class="background-img">
+            <img src="assets/pictures/background-santa.jpeg" alt="Santa-claus" class="w-100">
+        </div>
+    </section>
+    <section class="container-fluid">
+        <div class="p-sm-5">
+            <article>
+                <div>
+                    <h4>Sélection idées cadeaux</h4>
+                </div>
+                <div id="carouselExampleControls" class="pt-3 carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="d-block w-100 d-flex align-items-baseline justify-content-sm-between flex-sm-row flex-sm-nowrap flex-column" src="..." alt="First slide">
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/ipad-pro.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/lego.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/montre.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/apareil-photo.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/go-pro.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/asterix.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="d-block w-100 d-flex align-items-baseline justify-content-sm-between flex-sm-row flex-sm-nowrap flex-column" src="..." alt="Second slide">
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/ipad-pro.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/lego.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/montre.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/apareil-photo.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/go-pro.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/asterix.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="d-block w-100 d-flex align-items-baseline justify-content-sm-between flex-sm-row flex-sm-nowrap flex-column-reverse" src="..." alt="Third slide">
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/ipad-pro.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/lego.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/montre.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/apareil-photo.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/go-pro.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                                <a href="#" class="col-lg-2 col-md-4">
+                                    <div><img src="assets/pictures/asterix.jpg" class="w-100" alt="drone"></div>
+                                    <div>
+                                        <h6>GoPro Drone Karma</h6>
+                                        <p>1329 EUR</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </article>
+        </div>
+    </section>
+    <?php
+        include ("views/footer.php")
+    ?>
+</body>
+</html>
