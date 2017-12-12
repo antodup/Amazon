c778e022b25cedc4d84305e3928e540d0d28e62b
MatthieuLemaire <lemairematthieudecergywanadoo.fr>
2017-12-12 11:40:52 +0100

    ajout recherche.php et css

---
+++ views/recherche.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--TYPOGRAPHIES-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+          rel="stylesheet">
+
+    <script src="https://use.fontawesome.com/75a87d192f.js"></script>
+
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/recherche.css">
+    <title>Amazon</title>
+</head>
+<body>
+<?php
+include("header.php")
+?>
+<section class="d-flex justify-content-center align-items-center vh100 flex-column p-5 background-img-home">
+    <div class="col-sm-6 col-md-8">
+        <div class="input-group shadow">
+            <input type="text" class="form-control" placeholder="Search for..." aria-label="Search for...">
+            <span class="input-group-btn">
+                <button class="btn btn-secondary" type="button"><i class="fa fa-search"></i></button>
+            </span>
+        </div>
+</section>
+<section class="container p-5 bg-white">
+    <div class="d-flex justify-content-between flex-sm-nowrap">
+        <div class=""><h6>résultats pour....</h6></div>
+        <div class="form-group col-sm-3 d-flex row flex-nowrap justify-content-around">
+            <label for="exampleFormControlSelect1">trier par</label>
+            <select class="form-control-sm" id="exampleFormControlSelect1">
+                <option>Pertinence</option>
+                <option>Popularité</option>
+                <option>Prix croissants </option>
+                <option>Prix décroissants</option>
+                <option>Note moyenne</option>
+                <option>Nouveautés</option>
+            </select>
+        </div>
+    </div>
+    <article class="d-flex row flex-sm-wrap">
+        <div class="card m-3" style="width: 20rem;">
+            <img class="card-img-top" src="../assets/pictures/ipad-pro.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Drone kappa 1337</h5>
+                <h6>EUR 1337.99</h6>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <a href="#" class="btn btn-primary">j'achète</a>
+                </div>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 20rem;">
+            <img class="card-img-top" src="../assets/pictures/drone.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Drone kappa 1337</h5>
+                <h6>EUR 1337.99</h6>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <a href="#" class="btn btn-primary">j'achète</a>
+                </div>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 20rem;">
+            <img class="card-img-top" src="../assets/pictures/asterix.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Drone kappa 1337</h5>
+                <h6>EUR 1337.99</h6>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <a href="#" class="btn btn-primary">j'achète</a>
+                </div>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 20rem;">
+            <img class="card-img-top" src="../assets/pictures/montre.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Drone kappa 1337</h5>
+                <h6>EUR 1337.99</h6>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <a href="#" class="btn btn-primary">j'achète</a>
+                </div>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 20rem;">
+            <img class="card-img-top" src="../assets/pictures/go-pro.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Drone kappa 1337</h5>
+                <h6>EUR 1337.99</h6>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <a href="#" class="btn btn-primary">j'achète</a>
+                </div>
+            </div>
+        </div>
+        <div class="card m-3" style="width: 20rem;">
+            <img class="card-img-top" src="../assets/pictures/lego.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Drone kappa 1337</h5>
+                <h6>EUR 1337.99</h6>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <a href="#" class="btn btn-primary">j'achète</a>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
+
+<?php
+include("footer.php")
+?>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
+        integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
+        integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+        crossorigin="anonymous"></script>
+
+</body>
+</html>
